0061afd8e372a5255199aee453f942cf9b14a305
heloisebastos <heloisebastos28@hotmail.com>
2024-05-29 22:07:57 -0400

    :sparkles: feat: Adiciona o endpoint filmes

--- olabiflix/olabiflix/src/main/java/com/olabi/olabiflix/OlabiflixApplication.java
+++ olabiflix/olabiflix/src/main/java/com/olabi/olabiflix/OlabiflixApplication.java
@@ -1,13 +1,24 @@
 package com.olabi.olabiflix;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.olabi.olabiflix.model.entity.Filme;
+import com.olabi.olabiflix.repository.FilmeRepository;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @RestController
 public class OlabiflixApplication {
+	private final FilmeRepository repository;
+
+	public OlabiflixApplication(FilmeRepository repository) {
+		this.repository = repository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OlabiflixApplication.class, args);
@@ -16,6 +27,11 @@ public class OlabiflixApplication {
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello World!";
+	}
+
+	@GetMapping("/filmes")
+	public List<Filme> getFilmes() {
+		return repository.findAll();
 	}
 
 }
