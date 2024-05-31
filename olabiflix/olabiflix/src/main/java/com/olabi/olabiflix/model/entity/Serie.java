0bc14cc6c29ff65a7c12d03884c2796e8750aad0
heloisebastos <heloisebastos28@hotmail.com>
2024-05-31 10:26:29 -0400

    :sparkles: feat: Adiciona a entity Serie

---
+++ olabiflix/olabiflix/src/main/java/com/olabi/olabiflix/model/entity/Serie.java
@@ -0,0 +1,114 @@
+package com.olabi.olabiflix.model.entity;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.hibernate.annotations.UuidGenerator;
+
+import com.olabi.olabiflix.model.value.Ratings;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "serie")
+public class Serie {
+    @Id
+    @UuidGenerator
+    private UUID id;
+    private String title;
+    private String totalSeasons;
+    private List<String> genre;
+    private List<String> writers;
+    private String poster;
+    private List<String> actors;
+    @Embedded
+    private Ratings ratings;
+
+    protected Serie() {
+    }
+
+    public Serie(String title, String totalSeasons, List<String> genre, List<String> writers, String poster,
+            List<String> actors, Ratings ratings) {
+        this.title = title;
+        this.totalSeasons = totalSeasons;
+        this.genre = genre;
+        this.writers = writers;
+        this.poster = poster;
+        this.actors = actors;
+        this.ratings = ratings;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTotalSeasons() {
+        return totalSeasons;
+    }
+
+    public void setTotalSeasons(String totalSeasons) {
+        this.totalSeasons = totalSeasons;
+    }
+
+    public List<String> getGenre() {
+        return genre;
+    }
+
+    public void setGenre(List<String> genre) {
+        this.genre = genre;
+    }
+
+    public List<String> getWriters() {
+        return writers;
+    }
+
+    public void setWriters(List<String> writers) {
+        this.writers = writers;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public List<String> getActors() {
+        return actors;
+    }
+
+    public void setActors(List<String> actors) {
+        this.actors = actors;
+    }
+
+    public Ratings getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(Ratings ratings) {
+        this.ratings = ratings;
+    }
+
+    @Override
+    public String toString() {
+        return "Serie [id=" + id + ", title=" + title + ", totalSeasons=" + totalSeasons + ", genre=" + genre
+                + ", writers=" + writers + ", poster=" + poster + ", actors=" + actors + ", ratings=" + ratings + "]";
+    }
+
+}
