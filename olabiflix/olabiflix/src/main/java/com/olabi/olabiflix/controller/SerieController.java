35fc9add95e5d514570c1c64da4e8549f3231b45
heloisebastos <heloisebastos28@hotmail.com>
2024-06-01 18:28:38 -0400

    :sparkles: feat: Adiciona endpoint getId,delete e buscar

--- olabiflix/olabiflix/src/main/java/com/olabi/olabiflix/controller/SerieController.java
+++ olabiflix/olabiflix/src/main/java/com/olabi/olabiflix/controller/SerieController.java
@@ -1,15 +1,23 @@
 package com.olabi.olabiflix.controller;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.olabi.olabiflix.model.entity.Serie;
 import com.olabi.olabiflix.repository.SerieRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/series")
@@ -26,9 +34,40 @@ public class SerieController {
         return serieRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public Optional<Serie> getById(@PathVariable UUID id) {
+        return serieRepository.findById(id);
+    }
+
+    @GetMapping("/buscar-title")
+    public ResponseEntity<Serie> findByTitle(@RequestParam(name = "title", defaultValue = "") String title) {
+        Optional<Serie> serieEncontrada = serieRepository.findByTitle(title);
+
+        if (serieEncontrada.isPresent()) {
+            Serie serie = serieEncontrada.get();
+            return ResponseEntity.ok(serie);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+    }
+
+    @GetMapping("/buscar-genre")
+    public ResponseEntity<List<Serie>> findByGenre(@RequestParam(name = "genre", defaultValue = "") String genre) {
+        List<Serie> serie = serieRepository.findByGenreContainsIgnoreCase(genre);
+        return ResponseEntity.ok(serie);
+
+    }
+
     @PostMapping("/criar")
     public Serie createSerie(@RequestBody Serie serieBody) {
         return serieRepository.save(serieBody);
+    }
+
+    @DeleteMapping("{id}/delete")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delte(@PathVariable UUID id) {
+        serieRepository.deleteById(id);
     }
 
 }
