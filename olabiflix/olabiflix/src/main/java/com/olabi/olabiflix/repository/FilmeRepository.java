0b435f176a81247e92f51098d6f8573e492bb725
heloisebastos <heloisebastos28@hotmail.com>
2024-06-01 18:29:46 -0400

    :sparkles: feat: Adiciona endpoint getId,delete e buscar

--- olabiflix/olabiflix/src/main/java/com/olabi/olabiflix/repository/FilmeRepository.java
+++ olabiflix/olabiflix/src/main/java/com/olabi/olabiflix/repository/FilmeRepository.java
@@ -1,5 +1,7 @@
 package com.olabi.olabiflix.repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +10,9 @@ import com.olabi.olabiflix.model.entity.Filme;
 
 public interface FilmeRepository extends JpaRepository<Filme, UUID> {
 
-    // defini os metodos que vão gerar as queries
+    Optional<Filme> findByTitle(String title);
 
+    List<Filme> findByGenreContainsIgnoreCase(String genre);
+
+    List<Filme> findByTitleOrActors(String title, String actors);
 }
--- olabiflix/olabiflix/src/main/java/com/olabi/olabiflix/repository/SerieRepository.java
+++ olabiflix/olabiflix/src/main/java/com/olabi/olabiflix/repository/SerieRepository.java
@@ -1,5 +1,7 @@
 package com.olabi.olabiflix.repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +9,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.olabi.olabiflix.model.entity.Serie;
 
 public interface SerieRepository extends JpaRepository<Serie, UUID> {
+
+    Optional<Serie> findByTitle(String title);
+
+    List<Serie> findByGenreContainsIgnoreCase(String genre);
 
 }
